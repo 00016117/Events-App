c35205f1ae779b9fe24762c0845dc0badbd0cfdb
00016117 <mirjalol0111@gmail.com>
2024-03-13 16:47:18 +0500

    new style+linked with frontend

--- routes/api/event/index.js
+++ routes/api/event/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const { addEventValidation } = require('../../../validators/event');
 
 const router = express.Router();
-const ticket_controller = require('../../../controllers/api/event');
+const event_controller = require('../../../controllers/api/event');
 
 // Define API routes
 router.get('/', (req, res)=>{
-    ticket_controller.getAll(req, res);
+    event_controller.getAll(req, res);
 });
+
+router.post('/', addEventValidation(), (req, res)=>{
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    event_controller.create(req, res)
+})
 
 module.exports = router;
